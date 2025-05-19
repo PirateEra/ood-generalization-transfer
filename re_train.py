2037b5f36c0fdfeec2bf1bc26545264717d7dc79
Udder <pirate-era@hotmail.com>
2025-05-20 00:25:44 +0200

    added tensorboard logging

--- re_train.py
+++ re_train.py
@@ -7,7 +7,7 @@ from transformers import AutoTokenizer
 from transformers import EarlyStoppingCallback
 from sklearn.metrics import f1_score, precision_score, recall_score
 import numpy as np
-from utils import convert_labels, tokenize_function, multi_label_metric, multi_class_metric, multi_variate_metric
+from utils import convert_labels, tokenize_function, multi_label_metric, multi_class_metric, multi_variate_metric, parse_checkpoint_string
 from transformers import DataCollatorWithPadding
 import json
 from math import ceil
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     print("loading training args...")
     dataset_name = params.dataset_path.split("/")[-1]
     training_args = TrainingArguments(
-        output_dir= f"{params.output_dir}/{dataset_name}/seed_{params.seed}_testsize_{params.test_size}_bs_{params.batch_size}",      
+        output_dir= f"{params.output_dir}/{dataset_name}/dataset_{dataset_name}_seed_{params.seed}_testsize_{params.test_size}_bs_{params.batch_size}",      
         eval_strategy="epoch",      # Evaluate at the end of each epoch
         save_strategy="epoch",
         learning_rate=params.lr,              
@@ -139,14 +139,15 @@ if __name__ == "__main__":
         weight_decay=params.weight_decay,                # Regularization, prevent the model from learning too large weights
         save_total_limit=2,               # Limit checkpoints to save space
         load_best_model_at_end=True,
-        logging_dir=params.logging_dir,             
+        logging_dir=f"{params.logging_dir}/{dataset_name}/dataset_{dataset_name}_seed_{params.seed}_testsize_{params.test_size}_bs_{params.batch_size}",             
         logging_steps=logging_steps,            # dynamically log 5 times per epoch    
         fp16=params.fp16,                        # Enable mixed precision for faster training
         seed=params.seed,
         lr_scheduler_type="linear",
         warmup_ratio=0.1,                        # First 10% the LR gradually increases from 0 to specified LR
         greater_is_better=greater_is_better,     # Based on the better metric, for loss for example greater is not better
-        metric_for_best_model = best_metric # This is a metric that decides what model performed the best during each epoch, based on the task
+        metric_for_best_model = best_metric, # This is a metric that decides what model performed the best during each epoch, based on the task
+        report_to="tensorboard"
     )
 
     trainer = Trainer(
@@ -159,7 +160,6 @@ if __name__ == "__main__":
         compute_metrics=metric,
         callbacks=[EarlyStoppingCallback(early_stopping_patience=2)], # if after 2 epochs no improvement is made, earlystop
     )
-
 
     #--------
     # Training
--- train.py
+++ train.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
     print("loading training args...")
     dataset_name = params.dataset_path.split("/")[-1]
     training_args = TrainingArguments(
-        output_dir= f"{params.output_dir}/{dataset_name}/seed_{params.seed}_testsize_{params.test_size}_bs_{params.batch_size}",      
+        output_dir= f"{params.output_dir}/{dataset_name}/dataset_{dataset_name}_seed_{params.seed}_testsize_{params.test_size}_bs_{params.batch_size}",      
         eval_strategy="epoch",      # Evaluate at the end of each epoch
         save_strategy="epoch",
         learning_rate=params.lr,              
@@ -118,14 +118,15 @@ if __name__ == "__main__":
         weight_decay=params.weight_decay,                # Regularization, prevent the model from learning too large weights
         save_total_limit=2,               # Limit checkpoints to save space
         load_best_model_at_end=True,
-        logging_dir=params.logging_dir,             
+        logging_dir=f"{params.logging_dir}/{dataset_name}/dataset_{dataset_name}_seed_{params.seed}_testsize_{params.test_size}_bs_{params.batch_size}",             
         logging_steps=logging_steps,            # dynamically log 5 times per epoch    
         fp16=params.fp16,                        # Enable mixed precision for faster training
         seed=params.seed,
         lr_scheduler_type="linear",
         warmup_ratio=0.1,                        # First 10% the LR gradually increases from 0 to specified LR
         greater_is_better=greater_is_better,     # Based on the better metric, for loss for example greater is not better
-        metric_for_best_model = best_metric # This is a metric that decides what model performed the best during each epoch, based on the task
+        metric_for_best_model = best_metric, # This is a metric that decides what model performed the best during each epoch, based on the task
+        report_to="tensorboard"
     )
 
     trainer = Trainer(
